20b6a7e7dfdd4f1e17096d36b8bf7494a0d35cb6
Thomas Fromherz <thomas.fromherz@hotmail.com>
2021-05-12 13:02:46 +0200

    implemented regfile

--- miriv/vhdl/regfile.vhd
+++ miriv/vhdl/regfile.vhd
@@ -18,6 +18,55 @@ entity regfile is
 end entity;
 
 architecture rtl of regfile is
+
+	type reg_type is array (0 to (2 ** REG_BITS) - 1) of data_type;
+	signal reg : reg_type := (others => (others => '0'));
+	signal rddata1_reg, rddata2_reg : data_type;
+
 begin
+
+	-- enables bypassing and ensures that reads from register x0 always return 0
+	-- bypassing: necessary to read registers that are written within the same clk cycle
+	detect_bypassing_and_x0_reads : process(all)
+	begin
+			rddata1 <= rddata1_reg;
+			rddata2 <= rddata2_reg;
+
+			if regwrite = '1' and stall = '0' then
+				if rdaddr1 = wraddr then
+					rddata1 <= wrdata;
+				end if;
+
+				if rdaddr2 = wraddr then
+					rddata2 <= wrdata;
+				end if;
+			end if;
+
+			if to_integer(unsigned(rdaddr1)) = 0 then
+				rddata1 <= (others => '0');
+			end if;
+			if to_integer(unsigned(rdaddr2)) = 0 then
+				rddata2 <= (others => '0');
+			end if;
+
+	end process;
+
+	sync : process(clk, res_n)
+	begin
+			if (res_n = '0') then
+				rddata1_reg <= (others => '0');
+				rddata2_reg <= (others => '0');
+
+			elsif rising_edge(clk) then
+
+				rddata1_reg <= reg(to_integer(unsigned(rdaddr1)));
+				rddata2_reg <= reg(to_integer(unsigned(rdaddr2)));
+
+				if regwrite = '1' and stall = '0' then
+					reg(to_integer(unsigned(wraddr))) <= wrdata;
+				end if;
+
+			end if;
+	end process;
 
 end architecture;
