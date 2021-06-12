2494db8622a48892474d8eb607950630bc10d284
Thomas Fromherz <thomas.fromherz@hotmail.com>
2021-06-12 13:15:50 +0200

    implemented fwd.vhd

--- miriv/vhdl/fwd.vhd
+++ miriv/vhdl/fwd.vhd
@@ -22,4 +22,25 @@ end entity;
 
 architecture rtl of fwd is
 begin
+
+	output : process(all)
+	begin
+		do_fwd <= '0';
+		val <= ZERO_DATA;
+		
+		--check for forwarding from writeback stage first
+		if reg_write_wb.write = '1' and reg_write_wb.reg /= ZERO_REG and reg_write_wb.reg = reg then
+			do_fwd <= '1';
+			val <= reg_write_wb.data;
+		end if;
+
+		-- check for forwarding from mem stage afterwards
+		-- ensures that the most recent value of the register is forwared
+		if reg_write_mem.write = '1' and reg_write_mem.reg /= ZERO_REG and reg_write_mem.reg = reg then
+			do_fwd <= '1';
+			val <= reg_write_mem.data;
+		end if;
+
+	end process;
+
 end architecture;
